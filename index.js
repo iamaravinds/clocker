e2b8cdc82318f9dc80a9ca0d93db11631550954a
Aravind S <aravindsankararaman@gmail.com>
2021-07-23 15:49:20 +0530

    updated all configs

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const _ = require('lodash');
 console.log('Clocking ...')
+const objA = {
+    nest1: {
+        nest2: {
+            log: 'test log'
+        }
+    }
+};
+const objB = _.cloneDeep(objA)
+console.log(objB);
